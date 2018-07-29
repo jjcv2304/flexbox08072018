e37ad6791ab390839cd3bc7e567d7b5998d5e1cb
DESKTOP-QDFBKJS\jjcv2 <jjcv2304@hotmail.com>
2018-07-29 18:33:50 +0200

    Allow dynamic flexbox configuration. Add table inside tab

--- src/app/flexboxtutorial/components/maincontent/flexboxpractice/flexboxsandbox/flexboxsandbox.component.ts
+++ src/app/flexboxtutorial/components/maincontent/flexboxpractice/flexboxsandbox/flexboxsandbox.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+
+enum ValueType {dropdown, number, string, bool }
+
+export interface FlexboxProperty {
+  name: string;
+  valueType: ValueType;
+  availableValues: string[];
+}
+
+const containerProperties: FlexboxProperty[] = [
+  {name: 'display' , valueType: ValueType.dropdown, availableValues: ['flex'] },
+  {name: 'flex-direction', valueType: ValueType.dropdown, availableValues: ['flex-direction: row'
+      , 'row-reverse' , 'column' , 'column-reverse']}
+];
+
+// const itemProperties: FlexboxProperty[] = [
+//   {name: 'order', valueType: ValueType.number, availableValues: null , value: '0' },
+//   {name: 'flex-grow', valueType: ValueType.number, availableValues: null , value: '0'}
+// ];
 
 @Component({
   selector: 'app-flexboxsandbox',
   templateUrl: './flexboxsandbox.component.html',
   styleUrls: ['./flexboxsandbox.component.scss']
 })
-export class FlexboxsandboxComponent implements OnInit {
+export class FlexboxsandboxComponent  implements OnInit {
+  displayedColumnsContainerProperties: string[] = ['name', 'value'];
+  dataSourceContainerProperties = new MatTableDataSource(containerProperties);
+  ValueType: ValueType;
+  // displayedColumnsItemProperties: string[] = ['name', 'value'];
+  // dataSourceItemProperties = new MatTableDataSource(itemProperties);
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
