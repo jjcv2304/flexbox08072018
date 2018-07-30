e4eb3f0aedf2dfb14c52272f16f71cc0e1b2e805
DESKTOP-QDFBKJS\jjcv2 <jjcv2304@hotmail.com>
2018-07-30 20:35:57 +0200

    Allow dynamic flexbox configuration. Adding properties table

--- src/app/flexboxtutorial/components/maincontent/flexboxpractice/flexboxsandbox/flexboxsandbox.component.ts
+++ src/app/flexboxtutorial/components/maincontent/flexboxpractice/flexboxsandbox/flexboxsandbox.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
 
-enum ValueType {dropdown, number, string, bool }
+export enum ValueType {dropdown, number, string, bool }
 
 export interface FlexboxProperty {
   name: string;
@@ -10,29 +10,83 @@ export interface FlexboxProperty {
 }
 
 const containerProperties: FlexboxProperty[] = [
-  {name: 'display' , valueType: ValueType.dropdown, availableValues: ['flex'] },
-  {name: 'flex-direction', valueType: ValueType.dropdown, availableValues: ['flex-direction: row'
-      , 'row-reverse' , 'column' , 'column-reverse']}
+  {
+    name: 'display',
+    valueType: ValueType.dropdown,
+    availableValues: ['flex']
+  }, {
+    name: 'flex-direction',
+    valueType: ValueType.dropdown,
+    availableValues: ['flex-direction: row', 'row-reverse', 'column', 'column-reverse']
+  }, {
+    name: 'flex-wrap',
+    valueType: ValueType.dropdown,
+    availableValues: ['nowrap', 'wrap', 'wrap-reverse']
+  }, {
+    name: 'flex-flow',
+    valueType: ValueType.string,
+    availableValues: null
+  }, {
+    name: 'justify-content',
+    valueType: ValueType.dropdown,
+    availableValues: ['flex-start' , 'flex-end' , 'center' , 'space-between' , 'space-around' , 'space-evenly']
+  }, {
+    name: 'align-items',
+    valueType: ValueType.dropdown,
+    availableValues: ['flex-start' , 'flex-end' , 'center' , 'baseline' , 'stretch']
+  }, {
+    name: 'align-content',
+    valueType: ValueType.dropdown,
+    availableValues: ['flex-start' , 'flex-end' , 'center' , 'space-between' , 'space-around', 'stretch']
+  }
 ];
 
-// const itemProperties: FlexboxProperty[] = [
-//   {name: 'order', valueType: ValueType.number, availableValues: null , value: '0' },
-//   {name: 'flex-grow', valueType: ValueType.number, availableValues: null , value: '0'}
-// ];
+const itemProperties: FlexboxProperty[] = [
+  {
+    name: 'order',
+    valueType: ValueType.number,
+    availableValues: null
+  }, {
+    name: 'flex-grow',
+    valueType: ValueType.number,
+    availableValues: null
+  }, {
+    name: 'flex-shrink',
+    valueType: ValueType.number,
+    availableValues: null
+  }, {
+    name: 'flex-basis',
+    valueType: ValueType.string,
+    availableValues: null
+  }, {
+    name: 'flex',
+    valueType: ValueType.string,
+    availableValues: null
+  }, {
+    name: 'align-self',
+    valueType: ValueType.dropdown,
+    availableValues: ['auto' , 'flex-start' , 'flex-end' , 'center' , 'baseline' ,  'stretch']
+  }
+];
 
 @Component({
   selector: 'app-flexboxsandbox',
   templateUrl: './flexboxsandbox.component.html',
   styleUrls: ['./flexboxsandbox.component.scss']
 })
-export class FlexboxsandboxComponent  implements OnInit {
-  displayedColumnsContainerProperties: string[] = ['name', 'value'];
+export class FlexboxsandboxComponent implements OnInit {
+  displayedColumnsContainerProperties: string[] = ['value'];
   dataSourceContainerProperties = new MatTableDataSource(containerProperties);
-  ValueType: ValueType;
-  // displayedColumnsItemProperties: string[] = ['name', 'value'];
-  // dataSourceItemProperties = new MatTableDataSource(itemProperties);
 
-  constructor() { }
+  get ValueType() {
+    return ValueType;
+  }
+
+  displayedColumnsItemProperties: string[] = [ 'value'];
+  dataSourceItemProperties = new MatTableDataSource(itemProperties);
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
